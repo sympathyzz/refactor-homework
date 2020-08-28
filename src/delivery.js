b357e70e7cae66d71296832c65437eb7aa102c9e
GAOGA4 <961977096@qq.com>
2020-08-28 13:05:35 +0800

    [deliver][Gavin] refactor:extract deliveryDateIsNotRush method

--- src/delivery.js
+++ src/delivery.js
@@ -1,49 +1,50 @@
-function deliveryDate (anOrder, isRush) {
-  if (isRush) {
-    return deliveryDateIsRush(anOrder);
-  }
-  else {
-    let deliveryTime;
-    if ([
-      'MA',
-      'CT',
-      'NY',
-    ].includes(anOrder.deliveryState)) {
-      deliveryTime = 2;
+function deliveryDate(anOrder, isRush) {
+    if (isRush) {
+        return deliveryDateIsRush(anOrder);
+    } else {
+        return deliveryDateIsNotRush(anOrder);
     }
-    else if ([
-      'ME',
-      'NH',
-    ].includes(anOrder.deliveryState)) {
-      deliveryTime = 3;
-    }
-    else {
-      deliveryTime = 4;
-    }
-    return anOrder.placedOn.plusDays(2 + deliveryTime);
-  }
 }
 
-function deliveryDateIsRush(anOrder){
-  let deliveryTime;
-  if ([
-    'MA',
-    'CT',
-  ].includes(anOrder.deliveryState)) {
-    deliveryTime = 1;
-  }
-  else if ([
-    'NY',
-    'NH',
-  ].includes(anOrder.deliveryState)) {
-    deliveryTime = 2;
-  }
-  else {
-    deliveryTime = 3;
-  }
-  return anOrder.placedOn.plusDays(1 + deliveryTime);
-}
+    function deliveryDateIsRush(anOrder) {
+        let deliveryTime;
+        if ([
+            'MA',
+            'CT',
+        ].includes(anOrder.deliveryState)) {
+            deliveryTime = 1;
+        } else if ([
+            'NY',
+            'NH',
+        ].includes(anOrder.deliveryState)) {
+            deliveryTime = 2;
+        } else {
+            deliveryTime = 3;
+        }
+        return anOrder.placedOn.plusDays(1 + deliveryTime);
+    }
 
-module.exports = {
-  deliveryDate
-}
+    function deliveryDateIsNotRush(anOrder){
+        let deliveryTime;
+        if ([
+            'MA',
+            'CT',
+            'NY',
+        ].includes(anOrder.deliveryState)) {
+            deliveryTime = 2;
+        }
+        else if ([
+            'ME',
+            'NH',
+        ].includes(anOrder.deliveryState)) {
+            deliveryTime = 3;
+        }
+        else {
+            deliveryTime = 4;
+        }
+        return anOrder.placedOn.plusDays(2 + deliveryTime);
+    }
+
+    module.exports = {
+        deliveryDate
+    }
