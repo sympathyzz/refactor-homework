3cc7a66c4305e6d96b9cfcb93d710b995cda1785
GAOGA4 <961977096@qq.com>
2020-08-28 13:02:02 +0800

    [deliver][Gavin] refactor:extract deliveryDateIsRush method

--- src/delivery.js
+++ src/delivery.js
@@ -1,53 +1,49 @@
 function deliveryDate (anOrder, isRush) {
   if (isRush) {
-      deliveryDateIsRush(anOrder)
+    return deliveryDateIsRush(anOrder);
   }
   else {
-      deliveryDateIsNotRush(anOrder);
+    let deliveryTime;
+    if ([
+      'MA',
+      'CT',
+      'NY',
+    ].includes(anOrder.deliveryState)) {
+      deliveryTime = 2;
+    }
+    else if ([
+      'ME',
+      'NH',
+    ].includes(anOrder.deliveryState)) {
+      deliveryTime = 3;
+    }
+    else {
+      deliveryTime = 4;
+    }
+    return anOrder.placedOn.plusDays(2 + deliveryTime);
   }
 }
 
 function deliveryDateIsRush(anOrder){
-    let deliveryTime;
-    if ([
-        'MA',
-        'CT',
-    ].includes(anOrder.deliveryState)) {
-        deliveryTime = 1;
-    }
-    else if ([
-        'NY',
-        'NH',
-    ].includes(anOrder.deliveryState)) {
-        deliveryTime = 2;
-    }
-    else {
-        deliveryTime = 3;
-    }
-    return anOrder.placedOn.plusDays(1 + deliveryTime);
-}
-
-function deliveryDateIsNotRush(anOrder){
-    let deliveryTime;
-    if ([
-        'MA',
-        'CT',
-        'NY',
-    ].includes(anOrder.deliveryState)) {
-        deliveryTime = 2;
-    }
-    else if ([
-        'ME',
-        'NH',
-    ].includes(anOrder.deliveryState)) {
-        deliveryTime = 3;
-    }
-    else {
-        deliveryTime = 4;
-    }
-    return anOrder.placedOn.plusDays(2 + deliveryTime);
+  let deliveryTime;
+  if ([
+    'MA',
+    'CT',
+  ].includes(anOrder.deliveryState)) {
+    deliveryTime = 1;
+  }
+  else if ([
+    'NY',
+    'NH',
+  ].includes(anOrder.deliveryState)) {
+    deliveryTime = 2;
+  }
+  else {
+    deliveryTime = 3;
+  }
+  return anOrder.placedOn.plusDays(1 + deliveryTime);
 }
 
 module.exports = {
-  deliveryDate,
+  deliveryDate
 }
