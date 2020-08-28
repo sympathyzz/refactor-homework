d926a4a8040a596fe409041d65aefd9fb6b52700
GAOGA4 <961977096@qq.com>
2020-08-28 17:57:23 +0800

    [rank][Gavin] test:add Test8: voyage zone east-indies and history hasChina and voyage length is 3

--- test/rankTest.js
+++ test/rankTest.js
@@ -147,25 +147,33 @@ rankTest('Test6: voyage zone east-indies and history hasChina and voyage length 
 })
 
 rankTest('Test7: voyage zone east-indies and history hasChina and voyage length is 13 and history length is 6',t => {
-  const voyage = {
-    zone: 'east-indies',
-    length: 13,
-  };
-  const history = [
-    {
-      zone: 'east-indies',
-      profit: 5,
-    },{
-      zone: 'west-indies',
-      profit: 15,
-    },{
-      zone: 'china',
-      profit: -2,
-    },
-    {
-      zone: 'west-africa',
-      profit: 7,
-    },{},{}
-  ];
-  t.is(3,voyageProfitFactor(voyage,history))
+    const voyage = {
+        zone: 'east-indies',
+        length: 13,
+    };
+    const history = [
+        {
+            zone: 'east-indies',
+            profit: 5,
+        },{
+            zone: 'west-indies',
+            profit: 15,
+        },{
+            zone: 'china',
+            profit: -2,
+        },
+        {
+            zone: 'west-africa',
+            profit: 7,
+        },{},{}
+    ];
+    t.is(3,voyageProfitFactor(voyage,history))
+})
+
+rankTest('Test8: voyage zone east-indies and history hasChina and voyage length is 3',t => {
+    const voyage = {
+        zone: 'east-indies',
+        length: 3,
+    };
+    t.is(5,voyageRisk(voyage))
 })
