33f7f4a4e01ee532b45e9c7fb81e4427264d6fc0
GAOGA4 <961977096@qq.com>
2020-08-28 12:35:40 +0800

    [deliver][Gavin] refactor:extract deliveryDateIsNotRush method

--- src/delivery.js
+++ src/delivery.js
@@ -1,43 +1,51 @@
 function deliveryDate (anOrder, isRush) {
   if (isRush) {
-    let deliveryTime;
-    if ([
-      'MA',
-      'CT',
-    ].includes(anOrder.deliveryState)) {
-      deliveryTime = 1;
-    }
-    else if ([
-      'NY',
-      'NH',
-    ].includes(anOrder.deliveryState)) {
-      deliveryTime = 2;
-    }
-    else {
-      deliveryTime = 3;
-    }
-    return anOrder.placedOn.plusDays(1 + deliveryTime);
+      deliveryDateIsRush(anOrder)
   }
   else {
+      deliveryDateIsNotRush(anOrder);
+  }
+}
+
+function deliveryDateIsRush(anOrder){
     let deliveryTime;
     if ([
-      'MA',
-      'CT',
-      'NY',
+        'MA',
+        'CT',
     ].includes(anOrder.deliveryState)) {
-      deliveryTime = 2;
+        deliveryTime = 1;
     }
     else if ([
-      'ME',
-      'NH',
+        'NY',
+        'NH',
     ].includes(anOrder.deliveryState)) {
-      deliveryTime = 3;
+        deliveryTime = 2;
     }
     else {
-      deliveryTime = 4;
+        deliveryTime = 3;
+    }
+    return anOrder.placedOn.plusDays(1 + deliveryTime);
+}
+
+function deliveryDateIsNotRush(anOrder){
+    let deliveryTime;
+    if ([
+        'MA',
+        'CT',
+        'NY',
+    ].includes(anOrder.deliveryState)) {
+        deliveryTime = 2;
+    }
+    else if ([
+        'ME',
+        'NH',
+    ].includes(anOrder.deliveryState)) {
+        deliveryTime = 3;
+    }
+    else {
+        deliveryTime = 4;
     }
     return anOrder.placedOn.plusDays(2 + deliveryTime);
-  }
 }
 
 module.exports = {
