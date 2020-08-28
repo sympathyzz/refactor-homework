0673aa58e6b549ef6417332c4960fbe131b8954d
GAOGA4 <961977096@qq.com>
2020-08-28 19:55:06 +0800

    [delivery][Gavin] refactor:change deliveryDateIsNotRush to calculateNotRushDeliveryDate and extract nearCity and farCity in calculateNotRushDeliveryDate

--- src/delivery.js
+++ src/delivery.js
@@ -2,45 +2,38 @@ function deliveryDate(anOrder, isRush) {
     if (isRush) {
         return calculateRushDeliveryDate(anOrder);
     } else {
-        return deliveryDateIsNotRush(anOrder);
+        return calculateNotRushDeliveryDate(anOrder);
     }
 }
 
-    function calculateRushDeliveryDate(anOrder) {
-        let deliveryTime=0;
-        const nearCity = ['MA', 'CT',];
-        const farCity = ['NY', 'NH',];
-        if (nearCity.includes(anOrder.deliveryState)) {
-            deliveryTime = 1;
-        } else if (farCity.includes(anOrder.deliveryState)) {
-            deliveryTime = 2;
-        } else {
-            deliveryTime = 3;
-        }
-        return anOrder.placedOn.plusDays(1 + deliveryTime);
+function calculateRushDeliveryDate(anOrder) {
+    let deliveryTime = 0;
+    const nearCity = ['MA', 'CT',];
+    const farCity = ['NY', 'NH',];
+    if (nearCity.includes(anOrder.deliveryState)) {
+        deliveryTime = 1;
+    } else if (farCity.includes(anOrder.deliveryState)) {
+        deliveryTime = 2;
+    } else {
+        deliveryTime = 3;
     }
+    return anOrder.placedOn.plusDays(1 + deliveryTime);
+}
 
-    function deliveryDateIsNotRush(anOrder){
-        let deliveryTime;
-        if ([
-            'MA',
-            'CT',
-            'NY',
-        ].includes(anOrder.deliveryState)) {
-            deliveryTime = 2;
-        }
-        else if ([
-            'ME',
-            'NH',
-        ].includes(anOrder.deliveryState)) {
-            deliveryTime = 3;
-        }
-        else {
-            deliveryTime = 4;
-        }
-        return anOrder.placedOn.plusDays(2 + deliveryTime);
+function calculateNotRushDeliveryDate(anOrder) {
+    let deliveryTime;
+    const nearCity = ['MA', 'CT', 'NY',]
+    const farCity = ['ME', 'NH',]
+    if (nearCity.includes(anOrder.deliveryState)) {
+        deliveryTime = 2;
+    } else if (farCity.includes(anOrder.deliveryState)) {
+        deliveryTime = 3;
+    } else {
+        deliveryTime = 4;
     }
+    return anOrder.placedOn.plusDays(2 + deliveryTime);
+}
 
-    module.exports = {
-        deliveryDate
-    }
+module.exports = {
+    deliveryDate
+}
