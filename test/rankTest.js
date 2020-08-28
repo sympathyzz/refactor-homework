960484110fb0115e30bf8ff4bd3333979ae51498
GAOGA4 <961977096@qq.com>
2020-08-28 17:44:14 +0800

    [rank][Gavin] test:add Test4: voyage zone china and history hasChina and voyage length is 19 and history length is 11

--- test/rankTest.js
+++ test/rankTest.js
@@ -73,3 +73,28 @@ rankTest('Test3: voyage zone china and history hasChina and voyage length is 13 
   ];
   t.is(8,voyageProfitFactor(voyage,history))
 })
+
+rankTest('Test4: voyage zone china and history hasChina and voyage length is 19 and history length is 11',t => {
+  const voyage = {
+    zone: 'china',
+    length: 19,
+  };
+  const history = [
+    {
+      zone: 'east-indies',
+      profit: 5,
+    },{
+      zone: 'west-indies',
+      profit: 15,
+    },{
+      zone: 'china',
+      profit: -2,
+    },
+    {
+      zone: 'west-africa',
+      profit: 7,
+    },{},{},{},{},{},{},{}
+  ];
+  t.is(7,voyageProfitFactor(voyage,history))
+})
+
