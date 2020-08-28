8e5372f90a6eb8e66b539708100fa76ffb92e9a8
GAOGA4 <961977096@qq.com>
2020-08-28 11:49:46 +0800

    [print][Gavin] refactor:extract recordDueDate method

--- src/print.js
+++ src/print.js
@@ -1,37 +1,41 @@
-function printOwing (invoice) {
-  console.log('***********************');
-  console.log('**** Customer Owes ****');
-  console.log('***********************');
+function printOwing(invoice) {
+    console.log('***********************');
+    console.log('**** Customer Owes ****');
+    console.log('***********************');
 
-  const outstanding = calculateOutstanding(invoice);
+    const outstanding = calculateOutstanding(invoice);
 
-  // record due date
-  const today = new Date();
-  invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
+    // record due date
+    recordDueDate(invoice);
 
-  // print details
-  console.log(`name: ${invoice.customer}`);
-  console.log(`amount: ${outstanding}`);
-  console.log(`amount: ${invoice.dueDate.toLocaleDateString()}`);
+    // print details
+    console.log(`name: ${invoice.customer}`);
+    console.log(`amount: ${outstanding}`);
+    console.log(`amount: ${invoice.dueDate.toLocaleDateString()}`);
 
-  const logInfo='***********************\n'+
-      '**** Customer Owes ****\n'+
-      '***********************\n'+
-      `name: ${invoice.customer}\n`+
-      `amount: ${outstanding}\n`+
-      `amount: ${invoice.dueDate.toLocaleDateString()}`
-  return logInfo;
+    const logInfo = '***********************\n' +
+        '**** Customer Owes ****\n' +
+        '***********************\n' +
+        `name: ${invoice.customer}\n` +
+        `amount: ${outstanding}\n` +
+        `amount: ${invoice.dueDate.toLocaleDateString()}`
+    return logInfo;
 }
 
-function calculateOutstanding(invoice){
-    let outstanding=0;
+function calculateOutstanding(invoice) {
+    let outstanding = 0;
     for (const o of invoice.borderSpacing) {
         outstanding += o.amount;
     }
     return outstanding;
 }
 
+function recordDueDate(invoice) {
+    const today = new Date();
+    invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
+}
+
 module.exports = {
-  printOwing
+    printOwing
 }
 
